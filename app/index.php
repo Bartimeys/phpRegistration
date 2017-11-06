98630c2474fe20560a70181bd69f4dc74ed115f5
Bartimeus <bartimeusurucskiy@gmail.com>
2017-11-06 19:27:25 +0200

    add gulp-php, configure project

---
+++ app/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <!--    <link rel="manifest" href="site.webmanifest">-->
+    <link rel="apple-touch-icon" href="dev/img/assets/logo-loveknitting-sm.svg">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+  <!--build:css assets/css/styles.min.css-->
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <!--endbuild-->
+</head>
+<body>
+
+test
+
+<!--build:js assets/js/vendor.min.js -->
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<!-- endbuild -->
+
+<!--build:js assets/js/main.min.js -->
+<script src="assets/js/app.js"></script>
+<!-- endbuild -->
+<script>window.jQuery || document.write('<script src="dev/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
